c57ff36da2e77846a3b1c92cea24fa05118c2d9b
Air <airzhaoyn@gmail.com>
2020-02-22 22:26:30 +0800

    add util method

--- src/util/ListUtil.kt
+++ src/util/ListUtil.kt
@@ -1,4 +1,33 @@
 package util
 
+import common.ListNode
+
 object ListUtil {
+    @JvmStatic
+    fun buildList(array: IntArray): ListNode? {
+        if (array.isEmpty()) {
+            return null
+        }
+        val headValue = array[0] ?: return null
+
+        val head = ListNode(headValue)
+        var node = head
+        for (i in 1 until array.size) {
+            node.next = ListNode(array[i])
+            node = node.next!!
+        }
+        return head
+    }
+
+    @JvmStatic
+    fun print(head: ListNode?) {
+        if (head == null) {
+            return
+        }
+        var node: ListNode? = head
+        while (node != null) {
+            println(node.`val`)
+            node = node.next
+        }
+    }
 }
