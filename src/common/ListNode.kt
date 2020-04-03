8ebdc23245c6412d659a35cff037a042685a84d4
Air <airzhaoyn@gmail.com>
2020-04-04 07:49:54 +0800

    add toString

--- src/common/ListNode.kt
+++ src/common/ListNode.kt
@@ -5,4 +5,7 @@ package common
 class ListNode(@JvmField var `val`: Int) {
     @JvmField
     var next: ListNode? = null
+    override fun toString(): String {
+        return "ListNode(`val`=$`val`)"
+    }
 }
