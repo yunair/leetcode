afe5df80ee6617fa45fdfb3c978a0f6e6056102e
Air <airzhaoyn@gmail.com>
2020-12-13 15:55:32 +0800

    problem one day

--- src/tree/bst/P538ConvertBSTToGST.kt
+++ src/tree/bst/P538ConvertBSTToGST.kt
@@ -1,4 +1,51 @@
 package tree.bst
 
+import common.TreeNode
+import util.TreeUtil
+import java.util.*
+
+/**
+ * 把二叉搜索树转换为累加树
+ */
 object P538ConvertBSTToGST {
+    var max = 0
+    fun convertBST(root: TreeNode?): TreeNode? {
+        if (root == null) {
+            return null
+        }
+
+        convertBST(root.right)
+        root.`val` += max
+        max = root.`val`
+        convertBST(root.left)
+
+        return root
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        TreeUtil.printTreeNode(convertBST(TreeUtil.toTree(1, 0, 2)))
+        TreeUtil.printTreeNode(convertBST(TreeUtil.toTree(3, 2, 4, 1)))
+        TreeUtil.printTreeNode(
+            convertBST(
+                TreeUtil.toTree(
+                    4,
+                    1,
+                    6,
+                    0,
+                    2,
+                    5,
+                    7,
+                    null,
+                    null,
+                    null,
+                    3,
+                    null,
+                    null,
+                    null,
+                    8
+                )
+            )
+        )
+    }
 }
--- src/tree/level/P637AverageOfLevelsInBinaryTree.kt
+++ src/tree/level/P637AverageOfLevelsInBinaryTree.kt
@@ -1,4 +1,42 @@
 package tree.level
 
+import common.TreeNode
+import util.TreeUtil
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * 二叉树的层平均值
+ */
 object P637AverageOfLevelsInBinaryTree {
+    fun averageOfLevels(root: TreeNode?): DoubleArray {
+        if (root == null) {
+            return DoubleArray(1)
+        }
+        val queue = LinkedList<TreeNode>()
+        val list = ArrayList<Double>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            val size = queue.size
+            var value = 0.0
+            for (i in 0 until size) {
+                val node = queue.pop()
+                value += node.`val`
+                node.left?.let {
+                    queue.add(it)
+                }
+                node.right?.let {
+                    queue.add(it)
+                }
+            }
+            list.add(value / size)
+        }
+
+        return list.toDoubleArray()
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(averageOfLevels(TreeUtil.toTree(3, 9, 20, 15, 7)).contentToString())
+    }
 }
